398811fafa1a9737dcb5d540810bae56d771e2e8
Jonhels <jonhels8888@gmail.com>
2021-02-03 18:31:29 +0100

    added first content holder

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import './App.css';
 import Slider from './Components/Slider';
+import cat from './cat-back.png';
 
 function Home() {
   return (
-    <div >
+    <div>
       <div className="Header-h1">
         <h1>Home</h1>
       </div>
       <div className="Slider-div">
         <Slider></Slider>
       </div>
-
+      <div className="home-context">
+        <div className="information">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quibusdam ipsa natus corrupti minus eos porro amet, labore hic, blanditiis harum quidem cupiditate ipsam nam ad voluptatibus nesciunt sequi perspiciatis!Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quibusdam ipsa natus corrupti minus eos porro amet, labore hic, blanditiis harum quidem cupiditate ipsam nam ad voluptatibus nesciunt sequi perspiciatis!</p>
+          <a href="#">Contact</a>
+        </div>
+      </div>
+      <h1 className="Header-h1">Products</h1>
+      <div className="wrapper">
+      <div className="Content">
+          <div className="Emotes-holder">
+            <div className="holder-content">
+              <h1>Emotes</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam praesentium eos dignissimos? Fuga commodi esse debitis, quam beatae est impedit quae ab rerum sapiente dolorum autem qui eveniet sint obcaecati?</p>
+            </div>
+          </div>
+          <div className="img-holder">
+            <img className="cat-back" src={cat} alt="Cat-back" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
